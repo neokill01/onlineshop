1c07fae2052fa0877e369d0eeba0fceb33e2e18c
junyug <junyu010988@gmail.com>
2016-05-27 17:54:41 +0800

    项目更新，支持Log4j

--- app-controllers/src/main/java/com/onlineshop/controller/ProfileController.java
+++ app-controllers/src/main/java/com/onlineshop/controller/ProfileController.java
@@ -1,5 +1,7 @@
 package com.onlineshop.controller;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.helpers.Loader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,9 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/profile")
 public class ProfileController {
-
+    private Logger logger = Logger.getLogger(ProfileController.class);
     @RequestMapping(value="/index", method=RequestMethod.GET)
     public String actionIndex() {
+        logger.info("进入个人首页");
         return "profile/index";
     }
 }
