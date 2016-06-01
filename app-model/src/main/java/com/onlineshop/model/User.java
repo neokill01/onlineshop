74c749e333cfacafaef9c443cfb43e578ba12769
junyug <junyu010988@gmail.com>
2016-06-01 20:17:49 +0800

    编写user实体类

--- app-model/src/main/java/com/onlineshop/model/User.java
+++ app-model/src/main/java/com/onlineshop/model/User.java
@@ -1,30 +1,62 @@
 package com.onlineshop.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
- * Created by neo on 16/5/31.
+ * @name User
+ * @description 用户实体类
+ * @author junyuy
+ * @date 16/5/31
  */
 @Entity
-@Table
-public class User implements Serializable{
+@Table(name = "user")
+public class User implements Serializable {
+    private static final long servialVersionUID = 1L;
+    public User() {
+    }
+
+    public User(String nickname, String headImg, String email, String phone, String username, String password, Integer status, Integer createTime, Integer updateTime) {
+        this.nickname = nickname;
+        this.headImg = headImg;
+        this.email = email;
+        this.phone = phone;
+        this.username = username;
+        this.password = password;
+        this.status = status;
+        this.createTime = createTime;
+        this.updateTime = updateTime;
+    }
+
+    @Column(name = "id")
     @Id
-    @GeneratedValue
-    private int id;
-
-    @Size(min = 3)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "nickname")
     private String nickname;
+    @Column(name = "head_img")
+    private String headImg;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone", length = 45, unique = true)
+    private String phone;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "status", length = 11)
+    private Integer status;
+    @Column(name = "c_t")
+    private Integer createTime;
+    @Column(name = "u_t")
+    private Integer updateTime;
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -34,5 +66,85 @@ public class User implements Serializable{
 
     public void setNickname(String nickname) {
         this.nickname = nickname;
+    }
+
+    public String getHeadImg() {
+        return headImg;
+    }
+
+    public void setHeadImg(String headImg) {
+        this.headImg = headImg;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Integer createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Integer updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nickname='" + nickname + '\'' +
+                ", headImg='" + headImg + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", status=" + status +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                '}';
     }
 }
